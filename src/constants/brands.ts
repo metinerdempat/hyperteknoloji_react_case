d39745254826f9909baf15ebd968f4be2d1056e3
metinerdempat <metinerdempatweb@gmail.com>
2025-03-22 01:12:38 +0300

    Replace brands link with google.com

--- src/constants/brands.ts
+++ src/constants/brands.ts
@@ -3,7 +3,7 @@ const BRANDS = [
     id: 1,
     title: 'Avtocraska',
     image: '/brand-logos/avtocraska.svg',
-    url: 'https://avtocraska.ru',
+    url: 'https://google.com',
   },
   {
     id: 2,
@@ -21,46 +21,44 @@ const BRANDS = [
     id: 4,
     title: 'Enkei',
     image: '/brand-logos/enkei.svg',
-    url: 'https://enkei.com',
+    url: 'https://google.com',
   },
   {
     id: 5,
     title: 'Jurtrans',
     image: '/brand-logos/jurtrans.svg',
-    url: 'https://jurtrans.ru',
+    url: 'https://google.com',
   },
   {
     id: 6,
     title: 'Kikkoman',
     image: '/brand-logos/kikkoman.svg',
-    url: 'https://kikkoman.com',
+    url: 'https://google.com',
   },
   {
     id: 7,
     title: 'Salaspils',
     image: '/brand-logos/salaspils.svg',
-    url: 'https://salaspils.lv',
+    url: 'https://google.com',
   },
   {
     id: 8,
     title: 'Seibu',
     image: '/brand-logos/seibu.svg',
-    url: 'https://seibu.com',
+    url: 'https://google.com',
   },
   {
     id: 9,
     title: 'Tearfund',
     image: '/brand-logos/tearfund.svg',
-    url: 'https://tearfund.org',
+    url: 'https://google.com',
   },
   {
     id: 10,
     title: 'Unihold',
     image: '/brand-logos/unihold.svg',
-    url: 'https://unihold.ru',
+    url: 'https://google.com',
   },
 ] as const;
 
-
 export default BRANDS;
-
